76d8294a6991e568c3fe851eb68515d60aeb3b2c
bennyCoder <bennynkosi01@gmail.com>
2017-09-14 13:55:27 +0200

    kids_Profile Added
    kids profile hlaf done

---
+++ app/src/main/java/com/example/codetribe/my_kid/Kid.java
@@ -0,0 +1,15 @@
+package com.example.codetribe.my_kid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Kid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kid);
+
+
+    }
+}
