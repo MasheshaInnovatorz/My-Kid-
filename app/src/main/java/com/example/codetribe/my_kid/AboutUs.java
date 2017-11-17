8653769080689be986158c81fb1d889e98ee460a
mudau rudzani <mudaurrudzani@gmail.com>
2017-11-17 15:27:17 +0200

    about us
    about us

--- app/src/main/java/com/example/codetribe/my_kid/AboutUs.java
+++ app/src/main/java/com/example/codetribe/my_kid/AboutUs.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 public class AboutUs extends AppCompatActivity {
-
+//About
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
